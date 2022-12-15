51fc4f3b9f21d7c6f5a2ecc0393053e2e0045608
Allen Suárez <48367124+hub420@users.noreply.github.com>
2022-12-15 14:32:12 -0600

    Update settings.py

--- Aprender/settings.py
+++ Aprender/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'Aprender.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:aprenderestobase_user/Aprender',
+        default='postgresql://aprenderestobase/Aprender',
         conn_max_age=600       
     ) 
 }
